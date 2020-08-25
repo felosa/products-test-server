b2a83e96c5d690e3c0bc734cdbd999cf975dc710
felosa <fnandolopezsanchez@gmail.com>
2020-08-25 16:46:39 +0100

    trying to deploy

--- knexfile.js
+++ knexfile.js
@@ -1,22 +1,24 @@
 // Update with your config settings.
 
 module.exports = {
+  // development: {
+  //   client: "mysql",
+  //   connection: {
+  //     host: "127.0.0.1",
+  //     user: "root",
+  //     password: "Escocia150416",
+  //     database: "autius"
+  //   }
+  // },
+  // staging
   development: {
     client: "mysql",
     connection: {
       host: "127.0.0.1",
-      user: "root",
-      password: "Escocia150416",
-      database: "autius"
-    }
-  },
-  staging: {
-    client: "mysql",
-    connection: {
-      host: "127.0.0.1",
+      port: "3306",
       database: "autiuspruebas",
-      user: "autius",
-      password: "koEl86^6"
+      user: "autiuspruebas",
+      password: "desarrollo2020"
     }
   }
 };
