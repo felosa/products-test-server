c58784010e65287e1a9ee80abd6e6ad6c21877b9
felosa <fnandolopezsanchez@gmail.com>
2020-11-26 07:58:28 +0000

    archings views are working

--- api/archings.js
+++ api/archings.js
@@ -7,13 +7,77 @@ const {
   body,
 } = require("express-validator");
 const knex = require("../db/knex"); //the connection
-
+const moment = require("moment");
 const router = express.Router();
+
+const listingArchings = async (
+  res,
+  getQuery,
+  perPage,
+  page,
+  orderBy,
+  orderDir
+) => {
+  var totalCount = await getQuery
+    .clone()
+    .count("*", { as: "totalResults" })
+    .limit(999999)
+    .offset(0);
+
+  var results = await getQuery
+    .limit(perPage)
+    .offset((page - 1) * perPage)
+    .select(
+      "archings.id",
+      "archings.date",
+      "archings.tpv",
+      "archings.cash",
+      "archings.wireTransfer",
+      "archings.online",
+      "archings.withdrawals",
+      "archings.total",
+      "archings.idCenter"
+    );
+
+  return res.json({
+    page: page || 1,
+    perPage: perPage || 10,
+    totalCount: totalCount[0].totalResults,
+    results: results,
+  });
+};
+
+const exportArchings = async (res, getQuery, centerID) => {
+  var center = await knex("centers")
+    .select("centers.name as Nombre")
+    .where("centers.id", centerID);
+
+  var results = await getQuery.select(
+    "archings.date as Fecha",
+    "archings.tpv as TPV Manual",
+    "archings.online as TPV Online",
+    "archings.wireTransfer as Transferencia",
+    "archings.cash as Efectivo",
+    "archings.withdrawals as Retiradas",
+    "archings.total as Total"
+  );
+
+  const formatResult = results.map((elem) => {
+    elem["Fecha"] = moment(elem["Fecha"]).format("DD/MM/YYYY HH:mm:ss");
+    return elem;
+  });
+
+  return res.json({
+    centerName: `${center[0]["Nombre"]}`,
+    data: formatResult,
+  });
+};
 
 // GET ALL ARQUEOS
 router.get(
   "/",
   [
+    query("view").optional(),
     query("centerID").optional(),
     query("startDate").optional(),
     query("endDate").optional(),
@@ -30,6 +94,7 @@ router.get(
     }
 
     var {
+      view = "listing",
       centerID = null,
       startDate = null,
       endDate = null,
@@ -37,7 +102,6 @@ router.get(
       page = 1,
     } = req.query;
 
-    console.log(endDate, "endDate");
     var getQuery = knex
       .table("archings")
       .orderBy("archings.created_at", "desc");
@@ -53,33 +117,16 @@ router.get(
       getQuery.where("archings.date", "<=", new Date(endDate));
     }
 
-    var totalCount = await getQuery
-      .clone()
-      .count("*", { as: "totalResults" })
-      .limit(999999)
-      .offset(0);
+    switch (view) {
+      case "listing":
+        return listingArchings(res, getQuery, perPage, page);
 
-    var results = await getQuery
-      .limit(perPage)
-      .offset((page - 1) * perPage)
-      .select(
-        "archings.id",
-        "archings.date",
-        "archings.tpv",
-        "archings.cash",
-        "archings.wireTransfer",
-        "archings.online",
-        "archings.withdrawals",
-        "archings.total",
-        "archings.idCenter"
-      );
+      case "export":
+        return exportArchings(res, getQuery, centerID);
 
-    return res.json({
-      page: page || 1,
-      perPage: perPage || 10,
-      totalCount: totalCount[0].totalResults,
-      results: results,
-    });
+      default:
+        return res.status(404).send("View not found");
+    }
   }
 );
 
@@ -94,21 +141,41 @@ router.post("/", [body("centerID").toInt()], async (req, res) => {
 
   console.log(data, "centro llega");
 
+  // necesito el ultimo arqueo para saber apartir de que fecha y total hay que hacer el siguiente
+  const lastArching = await knex("archings")
+    .select("archings.date as lastArchingDate", "archings.total")
+    .where("archings.idCenter", data.centerID)
+    .orderBy("archings.date", "desc")
+    .first();
+
+  // si no hay ningun arching previo, cojo la fecha de creacion del centro y total es 0
+  const centerInfo = await knex("centers")
+    .select("centers.created_at")
+    .where("centers.id", data.centerID)
+    .first();
+
+  console.log(centerInfo, "centerInfo");
+
+  const lastArchingDate = lastArching
+    ? lastArching.lastArchingDate
+    : centerInfo.created_at;
+
+  const lastArchingTotal = lastArching ? lastArching.total : 0;
+
   const payments = await knex("payments")
     .leftJoin("students", "payments.idStudent", "students.id")
     .select("payments.paymentType", "payments.quantity")
     .where("students.idCenter", data.centerID)
-    .andWhere("payments.type", "Pago");
-  // .andWhere("payments.date", ">", "fecha de ultimo arqueo")
+    .andWhere("payments.type", "Pago")
+    .andWhere("payments.date", ">", lastArchingDate);
 
-  const closures = await knex("closures").select("closures.quantity");
-  // .where("closures.date", ">", "fecha ultimo arqueo");
+  const closures = await knex("closures")
+    .select("closures.quantity")
+    .where("closures.date", ">", lastArchingDate);
 
   const withdrawals = closures.reduce((total, elem) => {
     return elem.quantity + total;
   }, 0);
-
-  console.log(withdrawals, "total withdrawals");
 
   const paymentsSum = payments.reduce(
     (finalObject, elem) => {
@@ -124,18 +191,16 @@ router.post("/", [body("centerID").toInt()], async (req, res) => {
       "SUPERVISOR (Si es pack recuerda además reducir nº clases bolsa)": 0,
     }
   );
-
-  console.log(paymentsSum, "suma de todo");
-
+  console.log(lastArchingTotal, paymentsSum["Efectivo"], withdrawals, "dinero");
   knex("archings")
     .insert({
-      // date: new Date(),
+      date: new Date(),
       tpv: paymentsSum["TPV Manual"],
       cash: paymentsSum["Efectivo"],
       wireTransfer: paymentsSum["Transferencia"],
       online: paymentsSum["TPV Online"],
       withdrawals: withdrawals,
-      // total: 0 + paymentsSum["Efectivo"] - withdrawals,
+      total: lastArchingTotal + paymentsSum["Efectivo"] - withdrawals,
       idCenter: data.centerID,
       created_at: new Date(),
       updated_at: new Date(),
